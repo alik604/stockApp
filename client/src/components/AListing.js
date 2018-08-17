2057a4829dd570743387cb45dbd2067514ae85e9
khizr "Ali" Pardhan <alik9807274@gmail.com>
2018-08-16 22:32:40 -0700

    dfsf

--- client/src/components/AListing.js
+++ client/src/components/AListing.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, {Component} from 'react';
 import './AListing.css';
 
@@ -55,4 +56,63 @@ class AListing extends Component {
     }
 }
 
+=======
+import React, {Component} from 'react';
+import './AListing.css';
+
+class AListing extends Component {
+    constructor() {
+        super();
+        this.state = {
+            stocks: []
+        };
+
+    }
+
+    componentDidMount() {
+        fetch('Http://localhost:3001/MSFT')
+            .then(res => res.json())
+            .then(stock => {
+                    //  console.log(stock["Time Series (Daily)"]);
+
+
+                    var details = [];
+                    var dates = [];
+
+                    for (var x in stock["Time Series (Daily)"]) {
+                        details.push(parseFloat((stock["Time Series (Daily)"][x]['4. close'])));
+                        dates.push(x);
+                    }
+
+                    console.log(details);
+                    console.log(dates);
+
+                    var matrix = [dates, details];
+                    var myStock = stock["Time Series (Daily)"];
+                    this.setState(
+                        {myStock}
+                    )
+
+                }
+            );
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>MSFT stock</h2>
+                <ul>
+
+                    {this.state.myStock.forEach(stock => {
+                            console.log(stock);   //  <li key={stock.date}>on {stock.date}, closed at {stock.detail}</li>
+                        }
+                    )}
+
+                </ul>
+            </div>
+        );
+    }
+}
+
+>>>>>>> 4c7cf1752c48124e48a5fb61c6fefdbbb8566c51
 export default AListing;
